e7e42d5866fb34c90c37fca4f8256a8ba8b735b7
Huseein Omar <husseinomar6190@gmail.com>
2022-12-28 15:16:50 +0300

    intro to js

---
+++ app.js
@@ -0,0 +1,183 @@
+// console.log("HELLO FROM OUR FIRST JS FILE");
+// let total = 1+5
+// console.log("bye bye yooooh")
+
+//  Conditions js
+
+// console.log("its working");
+// console.log("before conditinals")
+// if (1+1===2){
+// console.log("correct math")
+// }
+// console.log("after conditinals")
+// let random=Math.random();
+// if(random<0.5) {
+// console.log("your number is less than 0.5!")
+// console.log(random);
+// }
+// let random=Math.random();
+// if(random=>0.5) {
+//  console.log("your number is greater than or equals to 0.5!")
+//  console.log(random);
+// }
+// const dayofweek=prompt("enter a day");
+// if(dayofweek==='Friday'){
+//  console.log("AAA I LOVE FRIDAYS!")
+// }
+// else if (dayofweek==="Monday"){
+//  console.log("UGG HATE MONDAYS!!!!")
+// }
+// else if (dayofweek==="Wednesday"){
+//  console.log(" wenesday just normal day!!")
+// }
+// else{
+//  console.log("meh")
+// }
+// const age =400
+// if(age <5){
+//  console.log("you are a baby,so its free")
+// }
+// else if(age <18){
+//  console.log("youre a kid ,so pay $10")
+// }
+// else if(age <65){
+//  console.log("youre a senior,so pay $20")
+// }
+// else{
+//  console.log("youre a senior,so pay $25")
+// }
+// const password=prompt("PLEASE ENTER A PASSWORD")
+// password must be 6+ characters
+// if (password.length >=8){
+//  if (password.indexOf(" ")===-1){
+//   console.log("GOOD! NO SPACE!")
+//  }
+//  else{
+//   console.log("PASSWORFD CANT CONTAIN SPACE")
+//  }
+// }
+// else{
+//  console.log("6+ CHARACTERS NEEDED!")
+// }
+// const userInpute=prompt("ENTER A PASSWORD")
+// if (userInpute){
+//  console.log("THRUTHY")
+// }
+// else{
+//  console.log("FALSEY")
+// }
+// if(0){
+//  console.log("TRUTHY!")
+// }
+// else{
+//  console.log("FALSEY!")
+// }
+// if(NaN){
+//  console.log("TRUTHY!")
+// }
+// else{
+//  console.log("FALSEY!")
+// }
+// 
+// if(' '){
+//  console.log("TRUTHY!")
+// }
+// else{
+//  console.log("FALSEY!")
+// }
+// const password=prompt("ENTER A PASSWORD")
+// if(password.lenght >= 6 && password.indexof (' ')=== -1){
+//  console.log("VALID PASSWORD!")
+// }
+// else{
+//  console.log("INCORRECT FORMAT FOR PASSWORD!")
+// }
+// const age=-20
+// if(age>=0&&age<5||age>=65){
+//  console.log("FREE!")
+// }
+// else if(age>=5 && age<10){
+//  console.log("$20")
+// }
+// else if(age>=10 && age<50){
+//  console.log("$40")
+// }
+// else{
+//  console.log("INVALID AGE!")
+// }
+// const firstname=prompt("ENTER YOUR NAME")
+// if(!firstname){
+//  firstname=prompt("PLZ TRY AGAIN!")
+// }
+// const age=0
+// if(age>=0&&age<5||age>=65){
+//  console.log("NOT YOUNG ENOUGH AND NOR OLD ENOUGH!!!")
+// }
+// const day=6
+// switch(day){
+//  case 1:
+//   console.log("UGG MONDAY MORNING!!!")
+//   // if break is not used it will just keep reading till the last code
+//   break;
+//   case 2:
+//   console.log("JUST TUESDAY!!!")
+//   break;
+//   case 3:
+//   console.log("WEDNESDAY!!!")
+//   break;
+//   case 4:
+//   console.log("YAY FASTING DAY THURSDAY!!!")
+//   break;
+//   case 5:
+//   console.log("HURRAY I LOVE FRIDAY!!!")
+//   break;
+//   case 6:
+//    case 7:
+//     console.log("WEEKEND!")
+//   break; 
+//   // and default is just like else
+//   default:
+//    console.log("teeeeh! i have no idea! btsstss!")
+// }
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log(6)
+// console.log(7)
+// console.log(8)
+// console.log(9)
+// console.log(10)
+// for( let i = 1; i <= 10; i++){
+//  console.log(i);
+// }
+// for( let siham = 1; siham <=6; siham++){
+ // console.log("IM COMING OVER" )
+ // console.log(siham);
+// }
+// for (let i = 100; i >= 0;i -=10 ){
+//  console.log(i);
+// }
+// for(let i = 10; i <= 1000; i *=10){
+//  console.log(i);
+// }
+// for(let s = 25; s >=0; s-=5){
+//  console.log(s)
+// }
+// let animals=["Cat",
+// "Zebra",
+// "Dog",
+// "Monkey",
+// "Rabbit",
+// "Snake",]
+// // for(let i = 0; i < animals.lenght; i++){
+// //  console.log(i, animals[i])
+// // } 
+// for(let i = animals.leng - 1; i <= 0; -1){
+//  console.log(animals[i])
+// } 
+// const people = ["scooby" , "velma" ,"daphne","shaggy","fred"]
+// for(let i = 0; i < people.length ; i++){
+//  console.log(people[i].toUpperCase());
+// }
---
+++ consoleCode.js
@@ -0,0 +1,188 @@
+// let gang=["Aisha","Asiya"]
+// undefined
+// gang.push("Fanka")
+// 3
+// gang
+// (3) ['Aisha', 'Asiya', 'Fanka']
+// let name="milhan";
+// undefined
+// name.toUpperCase
+// ƒ toUpperCase() { [native code] }
+// name.toUpperCase()
+// 'MILHAN'
+// gang.pop()
+// 'Fanka'
+// gang
+// (2) ['Aisha', 'Asiya']
+// gang.unshift("siham")
+// 3
+// gang
+// (3) ['siham', 'Aisha', 'Asiya']0: "siham"1: "Aisha"2: "Asiya"length: 3[[Prototype]]: Array(0)at: ƒ at()concat: ƒ concat()constructor: ƒ Array()copyWithin: ƒ copyWithin()entries: ƒ entries()every: ƒ every()fill: ƒ fill()filter: ƒ filter()find: ƒ find()findIndex: ƒ findIndex()findLast: ƒ findLast()findLastIndex: ƒ findLastIndex()flat: ƒ flat()flatMap: ƒ flatMap()forEach: ƒ forEach()includes: ƒ includes()indexOf: ƒ indexOf()join: ƒ join()keys: ƒ keys()lastIndexOf: ƒ lastIndexOf()length: 0map: ƒ map()pop: ƒ pop()push: ƒ push()reduce: ƒ reduce()reduceRight: ƒ reduceRight()reverse: ƒ reverse()shift: ƒ shift()slice: ƒ slice()some: ƒ some()sort: ƒ sort()splice: ƒ splice()toLocaleString: ƒ toLocaleString()toString: ƒ toString()unshift: ƒ unshift()values: ƒ values()Symbol(Symbol.iterator): ƒ values()Symbol(Symbol.unscopables): {at: true, copyWithin: true, entries: true, fill: true, find: true, …}[[Prototype]]: Object
+// gang.shift()
+// 'siham'
+// gang.shift()
+// 'Aisha'
+// gang.shift
+// ƒ shift() { [native code] }
+// gang.shift()
+// 'Asiya'
+// gang.unshift("pride fanka")
+// 1
+// gang.unshift("her sis asiya")
+// 2
+// gang.push("and the bestie aisha")
+// 3
+// gang.gang
+// undefined
+// gang
+// (3) ['her sis asiya', 'pride fanka', 'and the bestie aisha']
+// let planet=["The Moon","Venus","Earth","Mars","Jupiter",]
+// undefined
+// planet.shift()
+// 'The Moon'
+// planet
+// (4) ['Venus', 'Earth', 'Mars', 'Jupiter']
+// planet.pop("saturn")
+// 'Jupiter'
+// planet
+// (3) ['Venus', 'Earth', 'Mars']
+// planet.push("Jupiter")
+// 4
+// planet
+// (4) ['Venus', 'Earth', 'Mars', 'Jupiter']
+// planet.push("Saturn")
+// 5
+// planet
+// (5) ['Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn']
+// planet.unshift("Mercury")
+// 6
+// planet
+// (6) ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn']
+// let cat["catty","meowwwww"]
+// VM1879:1 Uncaught SyntaxError: Unexpected token '['
+// let cat=["catty","meowwwww"]
+// undefined
+// let dogs=["husky","the liitle ones"]
+// undefined
+// vats.concat(dog)
+// VM2079:1 Uncaught ReferenceError: vats is not defined
+//     at <anonymous>:1:1
+// (anonymous) @ VM2079:1
+// cat.concat(dogs)
+// (4) ['catty', 'meowwwww', 'husky', 'the liitle ones']
+// dogs.concat(cat)
+// (4) ['husky', 'the liitle ones', 'catty', 'meowwwww']
+// cat.indexof(m)
+// VM2283:1 Uncaught ReferenceError: m is not defined
+//     at <anonymous>:1:13
+// (anonymous) @ VM2283:1
+// cat.indexog('m')
+// VM2340:1 Uncaught TypeError: cat.indexog is not a function
+//     at <anonymous>:1:5
+// (anonymous) @ VM2340:1
+// cat.includes('meowwwww)
+// VM2439:1 Uncaught SyntaxError: Invalid or unexpected token
+//  dogs.concat(cat)
+// (4) ['husky', 'the liitle ones', 'catty', 'meowwwww']
+// let combo=dogs.concat(cat)
+// undefined
+// combo
+// (4) ['husky', 'the liitle ones', 'catty', 'meowwwww']
+// dogs.includes("husky')
+// VM2659:1 Uncaught SyntaxError: Invalid or unexpected token
+// dogs.includes('husky')
+// true
+// dogs.indexof('h')
+// VM2843:1 Uncaught TypeError: dogs.indexof is not a function
+//     at <anonymous>:1:6
+// (anonymous) @ VM2843:1
+// husky.indexof('h')
+// VM2921:1 Uncaught ReferenceError: husky is not defined
+//     at <anonymous>:1:1
+// (anonymous) @ VM2921:1
+// "husky".indexOf('u')
+// 1
+// combo.indexOf('the liitle ones')
+// 1
+// combo.reverse()
+// (4) ['meowwwww', 'catty', 'the liitle ones', 'husky']
+// combo
+// (4) ['meowwwww', 'catty', 'the liitle ones', 'husky']0: "meowwwww"1: "catty"2: "the liitle ones"3: "husky"length: 4[[Prototype]]: Array(0)
+// combo.unshift("luuv")
+// 5
+// combo
+// (5) ['luuv', 'meowwwww', 'catty', 'the liitle ones', 'husky']
+// combo.push("bulldog")
+// 6
+// combo
+// (6) ['luuv', 'meowwwww', 'catty', 'the liitle ones', 'husky', 'bulldog']
+// let idk=("pride","pridegroom","wow","wedding","blahblah","maogi")
+// undefined
+// let randomwords=("pride","pridegroom","wow","wedding","blahblah","maogi")
+// undefined
+// randomwords
+// 'maogi'
+// let words=["pride","pridegroom","wow","wedding","blahblah","maogi"]
+// undefined
+// words.slice(3)
+// (3) ['wedding', 'blahblah', 'maogi']
+// words.slice(0)
+// (6) ['pride', 'pridegroom', 'wow', 'wedding', 'blahblah', 'maogi']
+// words.slice(5)
+// ['maogi']
+// words.slice(-3)
+// (3) ['wedding', 'blahblah', 'maogi']
+// let lastwords.slice(-3)
+// VM4175:1 Uncaught SyntaxError: Unexpected token '.'
+// let last words=words.slice(-3)
+// VM4217:1 Uncaught SyntaxError: Unexpected identifier 'words'
+// let colours=["blue","yellow","black","green","white","brown"]
+// undefined
+// colours.splice(6,0,'purple')
+// []
+// colours
+// (7) ['blue', 'yellow', 'black', 'green', 'white', 'brown', 'purple']
+// let newWords = words.slice(4)
+// undefined
+// words
+// result
+// {siham: 86, milhan: 90}
+// result.milhan='b+'
+// 'b+'
+// result.siham='b'
+// 'b'
+// result.siham='b'
+// 'b'
+// result.siham='b' 
+// 'b'
+// result
+// {siham: 'b', milhan: 'b+'}
+// result.hinda='b-'
+// 'b-'
+// result['viruu']='c+'
+// 'c+'
+// result
+// {siham: 'b', milhan: 'b+', hinda: 'b-', viruu: 'c+'
+// let school = {
+//  name: "Ansarusuna",
+//  owener: "jir",
+//  numStudents: 200,
+//  isStadium: false
+// }
+
+// console.log(school.name);
+// console.log(school.numStudents);
+
+// let boss = school.name + ' ' + school.owener + ' ' + school.numStudents
+// console.log(boss);
+
+// let myName  = 'siham'
+
+// let newBoss = `${school.name} $myName is owned by ${school.owener} and it has this number of students ${school.numStudents}`
+// console.log(newBoss);
+
+// let num = 56
+
+// let xy = 'gvjgvhvyjh'
+
+// console.log(xy + num);
